27c325e8046a6f133cbfcd9fa84a37c5cde980f5
Lucas Meneghesso Faria <lucas.meneghesso.faria@gmail.com>
2021-12-14 10:21:28 -0300

    Completed test_utils_model.py

--- tests/test_utils_model.py
+++ tests/test_utils_model.py
@@ -1,14 +1,13 @@
-import pytest
 import gc
 import tensorflow as tf
 import sys
 import itertools
 from pathlib import Path
 try:
-    from utils.model import make_model, freeze_all_vgg, unfreeze_last_vgg
+    from utils.model import make_model, freeze_all_vgg, unfreeze_last_vgg, unfreeze_all_vgg, print_vgg_trainable
 except ModuleNotFoundError:
     sys.path.insert(0, str(Path(__file__).parent.parent.resolve()))
-    from utils.model import make_model, freeze_all_vgg, unfreeze_last_vgg
+    from utils.model import make_model, freeze_all_vgg, unfreeze_last_vgg, unfreeze_all_vgg, print_vgg_trainable
 
 
 def test_make_model():
@@ -33,3 +32,46 @@ def test_make_model():
         assert model.outputs[0].shape.as_list() == tf.TensorShape((None, params[0] if params[0] > 2 else 1)).as_list()
         del model
         gc.collect()
+
+
+@pytest.fixture
+def test_simple_model():
+    return make_model(n_classes=2, include_top_vgg=False, n_hidden=30, img_height=40, img_width=40)
+
+
+def test_freeze_all_vgg(test_simple_model):
+    model = test_simple_model
+    freeze_all_vgg(model)
+    for layer in model.layers:
+        if 'vgg' in layer.name:
+            for vgg_layer in layer.layers:
+                assert vgg_layer.trainable is False
+
+
+def test_unfreeze_last_vgg(test_simple_model):
+    model = test_simple_model
+    which_unfreeze = 10
+    unfreeze_last_vgg(model, which_unfreeze)
+    for layer in model.layers:
+        if 'vgg' in layer.name:
+            for vgg_layer in layer.layers[:which_unfreeze]:
+                assert vgg_layer.trainable is False
+            for vgg_layer in layer.layers[which_unfreeze:]:
+                assert vgg_layer.trainable is True
+    which_unfreeze = 15
+    unfreeze_last_vgg(model, which_unfreeze)
+    for layer in model.layers:
+        if 'vgg' in layer.name:
+            for vgg_layer in layer.layers[:which_unfreeze]:
+                assert vgg_layer.trainable is False
+            for vgg_layer in layer.layers[which_unfreeze:]:
+                assert vgg_layer.trainable is True
+
+
+def test_unfreeze_all_vgg(test_simple_model):
+    model = test_simple_model
+    unfreeze_all_vgg(model)
+    for layer in model.layers:
+        if 'vgg' in layer.name:
+            for vgg_layer in layer.layers:
+                assert vgg_layer.trainable is True
