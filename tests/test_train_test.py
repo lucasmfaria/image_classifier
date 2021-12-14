f2ba3d6051ae40e4628c2521d140a74bd551b6f4
Lucas Meneghesso Faria <lucas.meneghesso.faria@gmail.com>
2021-12-14 10:41:50 -0300

    Added support for test.py testing

--- tests/test_train_test.py
+++ tests/test_train_test.py
@@ -6,3 +6,9 @@ from pathlib import Path
 def test_train_command():
     p = subprocess.run(['python', str(Path(r'./scripts/train.py'))], shell=True, check=True)
     assert p.returncode == 0
+
+
+# TODO - define simple/dummy dataset to train only for testing
+def test_test_command():
+    p = subprocess.run(['python', str(Path(r'./scripts/test.py'))], shell=True, check=True)
+    assert p.returncode == 0
