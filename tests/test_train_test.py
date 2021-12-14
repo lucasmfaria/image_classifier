652048dc0cc3511f485d5b5c82eb68048c807b72
Lucas Meneghesso Faria <lucas.meneghesso.faria@gmail.com>
2021-12-14 10:46:10 -0300

    Deleting last train before training again

--- tests/test_train_test.py
+++ tests/test_train_test.py
@@ -2,6 +2,11 @@ import subprocess
 from pathlib import Path
 
 
+def test_delete_last_train_command():
+    p = subprocess.run(['python', str(Path(r'./scripts/delete_last_train.py'))], shell=True, check=True)
+    assert p.returncode == 0
+
+
 # TODO - define simple/dummy dataset to train only for testing
 def test_train_command():
     p = subprocess.run(['python', str(Path(r'./scripts/train.py'))], shell=True, check=True)
