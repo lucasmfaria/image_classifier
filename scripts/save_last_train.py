c8bdff2233933cd0a941fcd110330b644584eeb1
Lucas Meneghesso Faria <lucas.meneghesso.faria@gmail.com>
2022-01-20 21:34:51 -0300

    Created main() function on save_last_train.py

--- scripts/save_last_train.py
+++ scripts/save_last_train.py
@@ -1,29 +1,42 @@
 from pathlib import Path
 import os
 import shutil
-import subprocess
+import argparse
+import pandas as pd
+from test import main as test_main
 
-CHECKPOINTS_FOLDER = Path(__file__).resolve().parent.parent / 'models' / 'vgg16' / 'checkpoints'
-SAVED_MODELS_PATH = Path(__file__).resolve().parent.parent / 'models' / 'vgg16' / 'saved_models'
-TRAINED_MODEL_NAME = 'trained_weights'
+parser = argparse.ArgumentParser()
 
-existing_saved_models = [folder for folder in SAVED_MODELS_PATH.iterdir() if folder.is_dir()]
-new_saved_model_path = SAVED_MODELS_PATH / ('model_' + str(len(existing_saved_models)))
-os.mkdir(new_saved_model_path)  # create the new directory based on the number of older directories
-trained_model_file_paths = [file for file in CHECKPOINTS_FOLDER.iterdir() if TRAINED_MODEL_NAME in file.name]  # gets
-# the source paths to the trained model files
+# TODO - insert default values from dynaconf
+DEFAULT_WEIGHTS_PATH = Path(__file__).resolve().parent.parent / 'models' / 'vgg16' / 'checkpoints' / 'trained_weights'
+parser.add_argument('--weights_path', type=str, help='Path of the final model weights file',
+                    default=DEFAULT_WEIGHTS_PATH)
+DEFAULT_SAVED_MODELS_PATH = Path(__file__).resolve().parent.parent / 'models' / 'vgg16' / 'saved_models'
+parser.add_argument('--saved_models_path', type=str, help='Path of the saved models directory',
+                    default=DEFAULT_SAVED_MODELS_PATH)
 
-for file in trained_model_file_paths:  # copies the model weights files
-    shutil.copy(file, new_saved_model_path / file.name)
+args = parser.parse_args()
 
-# saves the test split statistics for the trained model
-if os.name == 'nt':
-    shell = True
-elif os.name == 'posix':
-    shell = False
-p = subprocess.run(['python', str(Path(r'./scripts/test.py'))], shell=shell, check=True, stdout=subprocess.PIPE)
 
-with open(new_saved_model_path / 'results.txt', 'w') as file_txt:
-    results_text = p.stdout.decode()
-    results_text = results_text[results_text.find('%') + 1:]
-    file_txt.write(results_text)
+def main(saved_models_path=DEFAULT_SAVED_MODELS_PATH, weights_path=DEFAULT_WEIGHTS_PATH):
+    existing_saved_models = [folder for folder in saved_models_path.iterdir() if folder.is_dir()]
+    new_saved_model_path = saved_models_path / ('model_' + str(len(existing_saved_models)))
+    os.mkdir(new_saved_model_path)  # create the new directory based on the number of older directories
+    trained_model_file_paths = [file for file in Path(weights_path).parent.iterdir() if
+                                Path(weights_path).name in file.name]  # gets the source paths to the trained model files
+
+    for file in trained_model_file_paths:  # copies the model weights files
+        shutil.copy(file, new_saved_model_path / file.name)
+
+    classification_report_dict, df_confusion_matrix = test_main(weights_path=Path(weights_path), return_results=True)
+
+    with open(new_saved_model_path / 'results.txt', 'w') as file_txt:
+        results_text = pd.DataFrame(classification_report_dict).T.to_string()
+        file_txt.write(results_text)
+        file_txt.write("\n\n")
+        results_text = df_confusion_matrix.to_string()
+        file_txt.write(results_text)
+
+
+if __name__ == '__main__':
+    main(saved_models_path=Path(args.saved_models_path), weights_path=Path(args.weights_path))
